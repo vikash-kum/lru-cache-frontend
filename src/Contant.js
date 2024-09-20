b3bda6deec404c6d30bb994e0ec9b40c2cc04df5
rentpaytech <rentpaytech@gmail.com>
2024-09-20 19:43:54 +0530

    added the build file

--- src/Contant.js
+++ src/Contant.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8000/";
-export const WEB_SOCKET_API_BASE_URL = "ws://localhost:8000/";
+export const API_BASE_URL = "http://3.0.0.167:8000/";
+export const WEB_SOCKET_API_BASE_URL = "ws://3.0.0.167:8000/";
